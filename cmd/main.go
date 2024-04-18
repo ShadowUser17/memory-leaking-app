5212b6f7a8b82434f4b69178a22cf76d389303be
ShadowUser17 <linuxforce17@gmail.com>
2024-04-18 13:15:24 +0300

    Update cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"crypto/rand"
+	"fmt"
+	"os"
+	"time"
+)
+
 func main() {
+	var buff = make([]byte, 4096)
+	var data = make([]byte, 0)
+	var ticker = time.NewTicker(time.Second)
+
+	for range ticker.C {
+		if _, err := rand.Read(buff); err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+
+		} else {
+			data = append(data, buff...)
+			fmt.Fprintf(os.Stdout, "Size: %d bytes\n", len(data))
+		}
+	}
 }
